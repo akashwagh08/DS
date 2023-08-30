5ba3bdfeac78b7371603d0203b64e78c421587e5
AKASH WAGH <137905104+akashwagh08@users.noreply.github.com>
2023-08-30 20:30:06 +0530

    Add files via upload

---
+++ insertend.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+ int data;
+ struct node *next;
+};
+struct node *create(struct node *list)
+{
+ int i,n;
+ struct node *newnode,*temp;
+ printf("Enter limit:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  newnode=(struct node*)malloc(sizeof(struct node));
+  printf("Enter value:");
+  scanf("%d",&newnode->data);
+  newnode->next=NULL;
+  if(list==NULL)
+  {
+    list=newnode;
+    temp=newnode; 
+ }
+ else
+ {
+  temp->next=newnode;
+  temp=newnode;
+ }
+ }
+ return list;
+}
+void disp(struct node *list)
+{
+  struct node *temp;
+  for(temp=list;temp!=NULL;temp=temp->next)
+  {
+    printf("%d\t",temp->data); 
+  } 
+}
+
+struct node* insertend(struct node *list,int num)
+{
+ struct node *newnode,*temp;
+ newnode=(struct node*)malloc(sizeof(struct node));
+ newnode->next=NULL;
+ newnode->data=num;
+ for(temp=list;temp->next!=NULL;temp=temp->next);
+ temp->next=newnode;
+ return list;
+}
+
+
+int main()
+{
+ int ch,num,pos;
+  struct node *list=NULL;
+  do
+  {
+   printf("\n 1-create \n 2-disp :");
+   printf("\n 3-insert End :");
+   printf("\n Enter choice:");
+   scanf("%d",&ch);
+   switch(ch)
+   {
+       case 1:list=create(NULL);
+            break;
+    case 2: disp(list);
+            break;
+     
+   case 3:printf("Enter number:");
+           scanf("%d",&num);
+     list=insertend(list,num);
+     break;
+   
+ }
+  }while(ch<4);
+}

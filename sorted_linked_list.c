58ab1acb36eda04c0595231eb10cbe499b323164
AKASH WAGH <137905104+akashwagh08@users.noreply.github.com>
2023-09-01 18:17:26 +0530

    Add files via upload

---
+++ sorted_linked_list.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+ int data;
+ struct node *next;
+}NODE;
+struct node *create(struct node *list)
+{
+ int i,n;
+ struct node *newnode,*temp;
+ printf("Enter limit:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  newnode=(struct node*)malloc(sizeof(struct node));
+  printf("Enter value:");
+  scanf("%d",&newnode->data);
+  newnode->next=NULL;
+  if(list==NULL)
+  {
+    list=newnode;
+    temp=newnode; 
+ }
+ else
+ {
+  temp->next=newnode;
+  temp=newnode;
+ }
+ }
+ return list;
+}
+void disp(struct node *list)
+{
+  struct node *temp;
+  for(temp=list;temp!=NULL;temp=temp->next)
+  {
+    printf("%d\t",temp->data); 
+  } 
+}
+NODE *sort(NODE *list)
+{
+ int t;
+ NODE *i,*j;
+    for(i=list;i!=NULL;i=i->next)
+    {
+      for(j=list;j!=i;j=j->next)
+    {
+     if(i->data>j->data)
+  {
+   t=i->data;
+   i->data=j->data;
+   j->data=t; 
+     } 
+ }
+  }
+  return list;
+}
+int main()
+{
+ NODE *list=NULL;
+ list=create(list);
+ printf("\n Display Linked List:");
+ disp(list);
+ list=sort(list);
+ printf("\n Display sorted Linked List:");
+ disp(list);
+}
